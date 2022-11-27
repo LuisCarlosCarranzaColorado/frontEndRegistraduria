2ee5846b2b42fed4f5f866a61eb8a7f88558a172
luis carlos <luiscarloscarranzacolorado>
2022-11-27 11:37:24 -0500

    se ajusta CORS

---
+++ src/app/pages/partidos/listar/listar.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Partido } from '../../../modelos/partido.model';
+import { PartidoService } from '../../../servicios/partido.service';
+
+@Component({
+  selector: 'ngx-listar',
+  templateUrl: './listar.component.html',
+  styleUrls: ['./listar.component.scss']
+})
+export class ListarComponent implements OnInit {
+  partidos : Partido[];
+  nombresColumnas: string[] = ['nombre','lema'];
+  constructor(private miServicioPartidos: PartidoService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.listar();
+  }
+  listar():void{
+    this.miServicioPartidos.listar().
+      subscribe(data => {
+        this.partidos=data;
+      });
+  }
+  agregar():void{
+    this.router.navigate(["pages/partidos/crear"]);
+  }
+  editar(id:string):void{
+    this.router.navigate(["pages/partidos/actualizar/"+id]);
+  }
+  eliminar(id:string):void{
+    Swal.fire({
+      title: 'Eliminar PARTIDO',
+      text: "Está seguro que quiere eliminar el partido?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.miServicioPartidos.eliminar(id).
+          subscribe(data => {
+            Swal.fire(
+              'Eliminado!',
+              'Partido ha sido eliminada correctamente',
+              'success'
+            )
+            this.ngOnInit();
+          });
+      }
+    })
+  }
+}
